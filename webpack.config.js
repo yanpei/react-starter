8d675f30df5c06f4b7c947e4f9b6446387e39755
DanDan <ddliu@thoughtworks.com>
2018-08-14 19:09:38 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,27 @@
-const path = require("path");
-const webpack = require("webpack");
-
+const path = require('path');
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  module: {
+  entry: './client/index.js', //打包的入口文件
+  output: {
+    path: path.resolve('dist'), // path.resolve():把相对路径转为绝对路径
+    filename: 'index_bundle.js' //打包完存储的文件地址与名称
+  },
+ module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: { presets: ['env'] }
+      { test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
       },
       {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }, {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
       }
     ]
-  },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [ new webpack.HotModuleReplacementPlugin() ]
-};
+  }
+}
